35684b61458ba83f6e058b8c69ccddc3e073b843
atran0802 <59665746+atran0802@users.noreply.github.com>
2020-02-12 21:45:07 -0700

    Update app.js

--- js-assignment3/code/app.js
+++ js-assignment3/code/app.js
@@ -1,31 +1,48 @@
-const fetchStarWars = () => {
-  const url = 'https://swapi.co/api/people/2'
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      document.getElementById('name').innerHTML = data.name
-      document.getElementById('height').innerHTML = data.height
-      document.getElementById('mass').innerHTML = data.mass
-      document.getElementById('hair_color').innerHTML = data.hair_color
-    })
-    
-}
-
-fetchStarWars()
-
-
-document.getElementById('luke').addEventListener ('click', luke)
-document.getElementById('c3po').addEventListener ('click', c3po)
-document.getElementById('r2').addEventListener ('click', r2)
-
-
-fetch(url)
-  .then(x => x.json())
-  .then(x => {
-    addToPage
-
-  })
-
-  function addToPage() {
-    
+const fetchPeople = () => {
+  const people = 'https://swapi.co/api/people/'
+  fetch(people)
+    .then(response => response.json())
+    .then(json => console.log(json))
   }
+
+const fetchPlanets = () => {
+  const planets = 'https://swapi.co/api/planets/'
+  fetch(planets)
+    .then(response => response.json())
+    .then(json => console.log(json))
+  }
+
+const fetchFilms = () => {
+  const films = 'https://swapi.co/api/films/'
+  fetch(films)
+    .then(response => response.json())
+    .then(json => console.log(json))
+  }
+
+const fetchSpecies = () => {
+  const species = 'https://swapi.co/api/species/'
+  fetch(species)
+    .then(response => response.json())
+    .then(json => console.log(json))
+  }
+
+const fetchVehicles = () => {
+  const vehicles = 'https://swapi.co/api/vehicles/'
+  fetch(vehicles)
+    .then(response => response.json())
+    .then(json => console.log(json))
+  }
+
+const fetchStarships = () => {
+  const starships = 'https://swapi.co/api/starships/'
+  fetch(starships)
+    .then(response => response.json())
+    .then(json => console.log(json))
+  }
+
+ document.getElementById('fetchPeople').addEventListener ('click', fetchPeople)
+ document.getElementById('fetchPlanets').addEventListener ('click', fetchPlanets)
+ document.getElementById('fetchFilms').addEventListener ('click', fetchFilms)
+ document.getElementById('fetchSpecies').addEventListener ('click', fetchSpecies)
+ document.getElementById('fetchVehicles').addEventListener ('click', fetchVehicles)
+ document.getElementById('fetchStarships').addEventListener ('click', fetchStarships)
